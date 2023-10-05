62cfdd7cffc0b75fe815fba187a4e7827ef70bec
Kristian Kirov <75641873+TheOptimisTT@users.noreply.github.com>
2023-10-05 23:16:57 +0300

    tinylampe patch

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
-
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").textContent = "🚩 Play 1 Wins!";
+var randomNumber1=6;
+var randomNumber2=6;
+function myMove(){
+  window.location.assign("./win.html");
 }
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+
+function roll(){
+  randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+  document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2); 
+
+ 
 }
-else {
-  document.querySelector("h1").textContent = "Draw!";
+function cmp(){
+  if (randomNumber1 > randomNumber2) {
+    
+    document.querySelector("h1").textContent = "🚩 Play 1 Wins!";
+    setTimeout(() => {  myMove(); }, 1500);
+    
+  }
+  else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+  }
+  else {
+    document.querySelector("h1").textContent = "Draw!";
+  }
+  
 }
+
 
